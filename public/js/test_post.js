a35a75bf4d61997ade512556917d84b2cb84f4e7
Bui Xuan Dinh <buixuandinh711@gmail.com>
2021-12-08 23:17:21 +0700

    add-test-code

---
+++ public/js/test_post.js
@@ -0,0 +1,14 @@
+var csrfToken = $("meta[name='csrf-token']").attr("content");  
+arr = [{code: "01", name : "Cầu Giấy"}, {code : "02", name : "Ba Đình"}, {code : "03", name : "Đống Đa"}];
+fetch('update-new-location', {
+    method: 'post',
+    headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": csrfToken
+    },
+    body: JSON.stringify(arr)
+  }).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    console.log(data);
+  });
--- resources/views/Main.blade.php
+++ resources/views/Main.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Home page</title>
     <link rel="StyleSheet" href="css/home_styles.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
