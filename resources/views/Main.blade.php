c0109eb9b223ddae8c103a98d1276bf3bcdf775c
Bui Xuan Dinh <buixuandinh711@gmail.com>
2021-12-16 00:01:49 +0700

    modify declare location, add general css

--- resources/views/Main.blade.php
+++ resources/views/Main.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Home page</title>
     <link rel="StyleSheet" href="css/home_styles.css">
+    <link rel="StyleSheet" href="css/general.css">
     <link rel="StyleSheet" href="css/declare_location.css">
     <link rel="StyleSheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
